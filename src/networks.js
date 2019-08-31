3107c37b557659f780742caad7ccb30b8d11896b
theboldproject <dev1@boldproject.io>
2019-08-30 18:07:36 -0600

    Audaxify1

--- src/networks.js
+++ src/networks.js
@@ -1,14 +1,14 @@
 module.exports = {
-  bitcoin: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bech32: 'bc',
+  audax: {
+    messagePrefix: '\x18Audax Signed Message:\n',
+    bech32: 'bo',
     bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
+      public: 0x022d2533,
+      private: 0x0221312b
     },
-    pubKeyHash: 0x3F,
-    scriptHash: 0x12,
-    wif: 0xBF
+    pubKeyHash: 0x17,
+    scriptHash: 0x8,
+    wif: 0x8E
   },
   testnet: {
     messagePrefix: '\x18Bitcoin Signed Message:\n',
